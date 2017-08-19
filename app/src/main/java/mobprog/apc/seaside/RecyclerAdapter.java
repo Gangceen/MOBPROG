f78656062e7a2c1dc79242a848fde457b2c5715f
Gabriel Angelo A. Ngceen <gngceen@gmail.com>
2017-08-19 15:09:03 +0800

    Edited

---
+++ app/src/main/java/mobprog/apc/seaside/RecyclerAdapter.java
@@ -0,0 +1,15 @@
+package mobprog.apc.seaside;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder>{
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public final View sView;
+        public final TextView list_entry_title;
+        public final TextView list_entry_content;
+        public 
+    }
+}
