43f307727659432367d7caf9282599ac61d56aa5
Gabriel Angelo A. Ngceen <gngceen@gmail.com>
2017-08-21 17:31:33 +0800

    Edits

--- app/src/main/java/mobprog/apc/seaside/RecyclerAdapter.java
+++ app/src/main/java/mobprog/apc/seaside/RecyclerAdapter.java
@@ -2,14 +2,26 @@ package mobprog.apc.seaside;
 
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
-
+/*
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder>{
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+
+
     public class ViewHolder extends RecyclerView.ViewHolder{
         public final View sView;
         public final TextView list_entry_title;
-        public final TextView list_entry_content;
-        public 
+        public final ImageButton list_entry_favorite;
     }
-}
+}*/
