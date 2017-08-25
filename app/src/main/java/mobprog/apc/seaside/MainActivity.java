ca3308790f162eabad2ad853c6bfcdf4085bc561
Gabriel Angelo A. Ngceen <gngceen@gmail.com>
2017-08-25 11:15:01 +0800

    Edit

--- app/src/main/java/mobprog/apc/seaside/MainActivity.java
+++ app/src/main/java/mobprog/apc/seaside/MainActivity.java
@@ -1,5 +1,6 @@
 package mobprog.apc.seaside;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Intent intent = getIntent();
+        String email = intent.getStringExtra(sign_in.User_Email);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -29,6 +33,9 @@ public class MainActivity extends AppCompatActivity
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+            View hView = navigationView.getHeaderView(0);
+            TextView userEmail = (TextView) hView.findViewById(R.id.textView2);
+            userEmail.setText(email);
         navigationView.setNavigationItemSelectedListener(this);
     }
 
--- app/src/main/java/mobprog/apc/seaside/sign_in.java
+++ app/src/main/java/mobprog/apc/seaside/sign_in.java
@@ -1,13 +1,31 @@
 package mobprog.apc.seaside;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class sign_in extends AppCompatActivity {
+    public static String User_Email = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        Button button = (Button) findViewById(R.id.button2);
+        final Intent intent = new Intent(this, MainActivity.class);
+        final EditText editText = (EditText) findViewById(R.id.editText1);
+
+        button.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        User_Email = editText.getText().toString();
+                        intent.putExtra(User_Email, User_Email);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
